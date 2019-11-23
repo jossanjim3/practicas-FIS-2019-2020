155f456327d298fd2a5c585eb63538ee0f7da002
Jose D. Sanchez <jossanjim3@gmail.com>
2019-11-23 16:42:25 +0000

    codigo limpio y listo para crear metodos put y delete con variable temporal contacts

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-// NOTA: NO FUNCIONA LA PARTE DE NEDB
-/*
-Es un problema de node, nedb no funciona en un container en windows
-He hablado de este problema con Manuel y me dijo de trabajar fuera del container mientras que utilizamos nedb
-*/
-
-// usado para hacer persistencia en fichero
-var DataStore = require('nedb');
-
 var port = 3000;
 var BASE_API_PATH = "/api/v1";
-// variable que indica la ruta donde se almacenaran los datos en fichero
-var DB_FILE_NAME = __dirname + "/contacts.json";
 
 console.log("Starting API server...");
 //console.log(DB_FILE_NAME);
@@ -28,13 +17,13 @@ app.get("/",(req, res) => {
 
 });
 
-/* // creamos una variable temporal contacts con los contactos de prueba
+ // creamos una variable temporal contacts con los contactos de prueba
 var contacts = [ 
     {"name" : "peter", "phone": 12345 }, 
     {"name" : "john", "phone": 6789} 
-]; */
+];
 
-/* // metodo GET usando variable temporal
+// metodo GET usando variable temporal
 app.get(BASE_API_PATH + "/contacts", (req, res) => {
     console.log(Date() + " - GET /contacts");
     res.send(contacts);
@@ -46,65 +35,7 @@ app.post(BASE_API_PATH + "/contacts", (req, res) => {
     var contact = req.body; //para que funcione esto tienes que añadir body-parser
     contacts.push(contact);
     res.sendStatus(201);
-}) */
-
-
-// usamos nedb para crear un BB.DD. en fichero
-// Type 3: Persistent datastore with automatic loading
-
-// creamos e inicializamos la BB.DD. en fichero
-// da error el autoload : true
-var db = new DataStore({
-    //filename : DB_FILE_NAME,
-    filename : "/workspace/contacts.json",
-    autoload: true
-});
-// otra forma de configurar la persistencia en fichero
-/* var db = new DataStore({
-    filename : DB_FILE_NAME,    
-});
-db.loadDatabase(function (err) {    // Callback is optional
-    // Now commands will be executed
-  });
-   */
-// You can issue commands right away
-
-// metodo GET usando variable temporal
-app.get(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + " - GET /contacts");
-    // como el filtro el vacio {} devuelve todos los elementos de los contactos
-    db.find({}, (err) => {
-        if (err) {
-            console.log(Date() + " - " + err);
-            res.sendStatus(500);
-        } else {
-            
-            //res.send(contacts); // devuelve todos los campos de los contactos
-
-            // elimina el elemento _id de la lista de los contactos
-            res.send(contacts.map((contact) => {
-                delete contact._id;
-                return contact;
-            }));
-        }
-    });
-});
-
-// metodo POST usando variable temporal
-app.post(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + " - POST /contacts");
-    var contact = req.body; //para que funcione esto tienes que añadir body-parser
-    //console.log(" - req.body => contact: " + contact);
-
-    db.insert(contact, function(err, record) {
-        if (err) {
-            console.log(Date() + " - " + err);
-            res.sendStatus(500);
-        } else {
-            res.sendStatus(201);
-        }
-    });    
-});
+}) 
 
 // metodo PUT usando variable temporal, actualiza registro y muestralo en consola
 app.put({
@@ -115,7 +46,6 @@ app.put({
 app.delete({
 
 });
-
 
 app.listen(port);
 
