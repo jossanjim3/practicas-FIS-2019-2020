ee7848187229a82dfdd8aff6e7a35dd187a28c75
Jose D. Sanchez <jossanjim3@gmail.com>
2019-11-27 13:24:36 +0000

    archivo data.js con datos, pero no se usa en ningun lado de momento

---
+++ data.js
@@ -0,0 +1,7 @@
+var contacts = [ 
+    {"id": 1, "name" : "peter", "phone": 12345, "activo": 1, "enable": 0}, 
+    {"id": 2, "name" : "john",  "phone": 54321, "activo": 0, "enable": 0}, 
+    {"id": 3, "name" : "esther",  "phone": 45678, "activo": 1, "enable": 0},
+    {"id": 4, "name" : "maria",  "phone": 56434, "activo": 0, "enable": 0},
+    {"id": 5, "name" : "paco",  "phone": 67687, "activo": 0, "enable": 0} 
+];
