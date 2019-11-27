3cc009b6a0f42b354480e4a7061a98d106697e63
jossanjim3 <jossanjim3@gmail.com>
2019-11-27 16:56:53 +0100

    ya funciona el POST en nedb

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+// usado para hacer persistencia en fichero
+var DataStore = require('nedb');
 
 // NOTA: NO FUNCIONA LA PARTE DE NEDB
 /*
-Es un problema de node, nedb no funciona en un container en windows
+Es un problema de node, nedb no funciona en un container remoto en windows con node.js y mongo.db
 He hablado de este problema con Manuel y me dijo de trabajar fuera del container mientras que utilizamos nedb
 */
-
-// usado para hacer persistencia en fichero
-var DataStore = require('nedb');
 
 var port = 3000;
 var BASE_API_PATH = "/api/v1";
@@ -21,53 +20,23 @@ console.log("Starting API server...");
 var app = express();
 app.use(bodyParser.json());
 
-// metodo root
-app.get("/",(req, res) => {
-    console.log(" - ROOT");
-    res.send("<html><body><h1>My server is running... JD</h1></body></html>");
-
-});
-
-/* // creamos una variable temporal contacts con los contactos de prueba
-var contacts = [ 
-    {"name" : "peter", "phone": 12345 }, 
-    {"name" : "john", "phone": 6789} 
-]; */
-
-/* // metodo GET usando variable temporal
-app.get(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + " - GET /contacts");
-    res.send(contacts);
-})
-
-// metodo POST usando variable temporal
-app.post(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + " - POST /contacts");
-    var contact = req.body; //para que funcione esto tienes que añadir body-parser
-    contacts.push(contact);
-    res.sendStatus(201);
-}) */
-
-
 // usamos nedb para crear un BB.DD. en fichero
 // Type 3: Persistent datastore with automatic loading
 
 // creamos e inicializamos la BB.DD. en fichero
 // da error el autoload : true
 var db = new DataStore({
-    //filename : DB_FILE_NAME,
-    filename : "/workspace/contacts.json",
+    filename : DB_FILE_NAME,
+    //filename : "/workspace/contacts.json",
     autoload: true
 });
-// otra forma de configurar la persistencia en fichero
-/* var db = new DataStore({
-    filename : DB_FILE_NAME,    
+
+// metodo root
+app.get("/",(req, res) => {
+    console.log(" - ROOT");
+    res.send("<html><body><h1>My server is running... JD</h1></body></html>");
+
 });
-db.loadDatabase(function (err) {    // Callback is optional
-    // Now commands will be executed
-  });
-   */
-// You can issue commands right away
 
 // metodo GET usando variable temporal
 app.get(BASE_API_PATH + "/contacts", (req, res) => {
@@ -101,12 +70,11 @@ app.post(BASE_API_PATH + "/contacts", (req, res) => {
             console.log(Date() + " - " + err);
             res.sendStatus(500);
         } else {
+            console.log(record._id);
             res.sendStatus(201);
         }
     });    
 });
-<<<<<<< HEAD
-=======
 
 // metodo PUT usando variable temporal, actualiza registro y muestralo en consola
 app.put({
@@ -118,7 +86,6 @@ app.delete({
 
 });
 
->>>>>>> CRUD-contacts
 
 app.listen(port);
 
