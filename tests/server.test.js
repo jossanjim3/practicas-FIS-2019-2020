3131eccd43287f3574d3c3574f29e1e32ab69483
jossanjim3 <jossanjim3@gmail.com>
2019-12-09 18:36:07 +0100

    pruebas get y post finalizadas

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,6 @@
 
 const app = require('../server.js');
+const db = require('../db.js');
 const request = require('supertest');
 
 describe ("Hello World Texts", () => {
@@ -14,6 +15,7 @@ describe ("Hello World Texts", () => {
 });
 
 describe("Contacts API", () => {
+    
     describe("GET /", () => {
         it ("Should return an HTML document", () => {
             return request(app).get("/").then((response) => {
@@ -23,4 +25,62 @@ describe("Contacts API", () => {
             });
         });
     });
+
+    describe("GET /contacts", () => {
+        
+        // parte arrange
+        beforeAll(() => {
+            const contacts = [
+                {name : "juan", phone: 5555},
+                {name : "pepe", phone: 6666}
+            ];
+
+            dbFind = jest.spyOn(db, "find");
+            dbFind.mockImplementation((query,callback) => {
+                callback(null, contacts);
+            });
+        });
+
+        it ("Should return all contacts", () => {
+            return request(app).get("/api/v1/contacts").then((response) => {
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toBeArrayOfSize(2);
+                expect(dbFind).toBeCalledWith({},expect.any(Function));
+            });
+        });
+    });
+
+    describe("POST /contacts", () => {
+        const contact = {name: "andres", phone:58965};
+        let dbInsert;
+
+        beforeEach(() => {
+            dbInsert = jest.spyOn(db, "insert");
+        });
+        
+        it ("Should add a new contact...", () => {
+            dbInsert.mockImplementation((c,callback) => {
+                callback(false);
+            });
+
+            return request(app).post("/api/v1/contacts").send(contact).then((response) => {
+                expect(response.statusCode).toBe(201);
+                expect(dbInsert).toBeCalledWith(contact, expect.any(Function));
+
+            });
+        });
+
+        it ("Should return 500 if there is a problem...", () => {
+            dbInsert.mockImplementation((c,callback) => {
+                callback(true);
+            });
+
+            return request(app).post("/api/v1/contacts").send(contact).then((response) => {
+                expect(response.statusCode).toBe(500);
+            });
+
+        });
+
+    });    
+
 });
