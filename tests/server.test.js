8e258dbb37552f38758cc1115861d2a61dc360a7
jossanjim3 <jossanjim3@gmail.com>
2019-12-09 17:53:39 +0100

    prueba sobre api con ruta /

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,4 +1,7 @@
 
+const app = require('../server.js');
+const request = require('supertest');
+
 describe ("Hello World Texts", () => {
     
     it("Should do an stupid test", () => {
@@ -7,5 +10,17 @@ describe ("Hello World Texts", () => {
         const sum = a+b;
 
         expect(sum).toBe(8);
+    });
+});
+
+describe("Contacts API", () => {
+    describe("GET /", () => {
+        it ("Should return an HTML document", () => {
+            return request(app).get("/").then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.type).toEqual(expect.stringContaining("html"));
+                expect(response.text).toEqual(expect.stringContaining("h1"));
+            });
+        });
     });
 });
